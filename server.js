7d22b00fb7c5d323baa401c51f735c1333e6b36e
sidalto <sidalto.pj@gmail.com>
2020-09-01 10:42:17 -0300

    atualização server

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Server is running in the porta ${port}`);
+  console.log(`Server is running in the port ${port}`);
 });
